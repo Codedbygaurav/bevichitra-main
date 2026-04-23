3983ccbfe6f45c83a437ede3a79c74bd97c9e179
codedbygaurav <gaurav.codes27@gmail.com>
2026-04-23 23:10:46 +0530

    renamed robot.js to robots.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import localFont from "next/font/local";
 import { ThemeProvider } from "next-themes";
 import { ContactProvider } from "@/context/ContactContext";
 import ContactPopupWrapper from "@/components/ui/ContactPopupWrapper";
+import Script from "next/script";
 
 const wildborn = localFont({
   src: "../../public/fonts/wildborn-Black.woff2",
@@ -54,10 +55,24 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${wildborn.variable} ${fredokaLight} ${fredokaBold} ${fredokaRegular}`}
+      className={`${wildborn.variable} ${fredokaLight.variable} ${fredokaBold.variable} ${fredokaRegular.variable}`}
       suppressHydrationWarning
     >
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-70NDWGC37C"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-70NDWGC37C');
+  `}
+        </Script>
+
         <ThemeProvider attribute="class">
           <NavProvider>
             <ContactProvider>
